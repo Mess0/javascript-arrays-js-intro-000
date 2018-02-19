ddd3e4ed78a5d00505f6c24c5f145f79d229ab78
Serhii <sergey.petrunya@gmail.com>
2018-02-19 17:35:48 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+const array = [1];
+
+var addElementToBeginningOfArray = (array, element) => {
+
+array = [element , ...array];
+
+  return array;
+}
+
+var destructivelyAddElementToBeginningOfArray = (array, element) =>{
+  array.unshift(element);
+  return array;
+}
